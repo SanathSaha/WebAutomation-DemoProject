27378215c550ed731286b4a34e4731312333c356
SanathSaha <sanath.saha007@gmail.com>
2025-05-18 11:35:26 +0600

    first commit

---
+++ src/main/java/me/sanathsaha/App.java
@@ -0,0 +1,13 @@
+package me.sanathsaha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
