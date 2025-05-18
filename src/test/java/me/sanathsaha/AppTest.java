88758f14756c45e94a6c1ab951220e82bc9873ff
SanathSaha <sanath.saha007@gmail.com>
2025-05-18 11:37:12 +0600

    Add WebAutomation

--- src/test/java/me/sanathsaha/AppTest.java
+++ src/test/java/me/sanathsaha/AppTest.java
@@ -1,38 +1,27 @@
 package me.sanathsaha;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends DriverSetup
+
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @Test
+    public void WebPageLoad () {
+        driver.get("https://google.com");
+        System.out.println(driver.getCurrentUrl());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void WebPageLoadTitle () {
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
     }
 }
