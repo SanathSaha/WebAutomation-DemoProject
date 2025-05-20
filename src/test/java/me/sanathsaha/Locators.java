fc226874c1711a79c390f0a9e1fd6838b6b92bd9
SanathSaha <sanath.saha007@gmail.com>
2025-05-20 23:30:05 +0600

    Locators add

--- src/test/java/me/sanathsaha/Locators.java
+++ src/test/java/me/sanathsaha/Locators.java
@@ -1,12 +1,33 @@
 package me.sanathsaha;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class Locators extends DriverSetup{
 
     @Test
-    public void testlocators(){
+    public void testlocators() throws InterruptedException {
         driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+
+       // WebElement searchArea = driver.findElement(By.id("APjFqb"));
+        //searchArea.sendKeys("movie");
+
+       // WebElement searchArea = driver.findElement(By.name("q"));
+        //searchArea.sendKeys("movie");
+
+        //WebElement searchArea = driver.findElement(By.tagName("textarea"));
+        //searchArea.sendKeys("movie");
+
+        //WebElement searchArea = driver.findElement(By.className("gLFyf"));
+        //searchArea.sendKeys("movie");
+
+        //driver.findElement(By.linkText("Gmail")).click();
+
+        driver.findElement(By.partialLinkText("How Search works")).click();
+
+        Thread.sleep(3000);
     }
 
 
