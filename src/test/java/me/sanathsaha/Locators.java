961be1567c3b70fb7c762db679f953e24d91d45f
SanathSaha <sanath.saha007@gmail.com>
2025-05-24 23:47:46 +0600

    New Locators

--- src/test/java/me/sanathsaha/Locators.java
+++ src/test/java/me/sanathsaha/Locators.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Locators extends DriverSetup{
 
     @Test
@@ -25,8 +27,21 @@ public class Locators extends DriverSetup{
 
         //driver.findElement(By.linkText("Gmail")).click();
 
-        driver.findElement(By.partialLinkText("How Search works")).click();
+        //WebElement searchArea = driver.findElement(By.partialLinkText("কীভাবে কাজ করে"));
+        //searchArea.click();
 
+
+        //WebElement searchArea = driver.findElement(By.cssSelector("textarea[name='q']"));
+        //searchArea.sendKeys("T-shirt");
+
+        //WebElement searchArea = driver.findElement(By.xpath("//textarea[@name='q']"));
+        //searchArea.sendKeys("T-shirt");
+
+        //List<WebElement> childs = driver.findElements(By.cssSelector("div[class='KxwPGc AghGtd']>a"));
+
+        //for(WebElement child: childs){
+            //System.out.println(child.getText());
+        //}
         Thread.sleep(3000);
     }
 
